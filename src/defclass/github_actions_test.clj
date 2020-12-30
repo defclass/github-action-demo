c99d71b870dc461c13aa6067b4f40a278e396a44
defclass <defclass@outlook.com>
2020-12-30 17:40:11 +0800

    Init project

---
+++ src/defclass/github_actions_test.clj
@@ -0,0 +1,7 @@
+(ns defclass.github-actions-test
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/defclass/github_actions_test_test.clj
@@ -0,0 +1,7 @@
+(ns defclass.github-actions-test-test
+  (:require [clojure.test :refer :all]
+            [defclass.github-actions-test :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
